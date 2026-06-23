427f3cba18c9803c393fb90eaf8d5e78fcd8e2bb
drei <drei@bitcoin.com>
2026-06-23 10:32:36 -0500

    refactor: rename product/target to BitcoinQRCodeGenerator to avoid QRCodeGenerator product collision with dagronf/swift-qrcode-generator

--- Package.swift
+++ Package.swift
@@ -4,28 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-qrcode-generator",
+    name: "SwiftQRCodeGenerator",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "QRCodeGenerator",
-            targets: ["QRCodeGenerator"]
+            name: "BitcoinQRCodeGenerator",
+            targets: ["BitcoinQRCodeGenerator"]
         )
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "QRCodeGenerator",
+            name: "BitcoinQRCodeGenerator",
+            path: "Sources/BitcoinQRCodeGenerator",
             dependencies: []
         ),
         .testTarget(
-            name: "QRCodeGeneratorTests",
-            dependencies: ["QRCodeGenerator"]
+            name: "BitcoinQRCodeGeneratorTests",
+            dependencies: ["BitcoinQRCodeGenerator"]
         )
     ]
 )
